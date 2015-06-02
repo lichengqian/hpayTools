2a3e3b64277b3ab8a1a26878f8d282a0ae73a087
mingli <john@ihaomo.com>
2015-06-02 10:43:11 +0800

    tcpShare / tcpConnect

--- src/ClientMain.hs
+++ src/ClientMain.hs
@@ -16,9 +16,9 @@ data HpayCtl =    LogProvider  {hostport :: HostPort
                             ,level :: String
                 }
              | LogMonitor   {hostport :: HostPort, level :: String}
-             | TcpSkeleton  {hostport :: HostPort,
+             | TcpShare  {hostport :: HostPort,
                              proxy :: String, level :: String }
-             | TcpStub      {hostport :: HostPort,
+             | TcpConnect      {hostport :: HostPort,
                              proxy :: String, level :: String }
                deriving (Data, Typeable, Show, Eq)
 
@@ -32,20 +32,20 @@ logMonitor = LogMonitor {
   , level = "INFO" &= help "log level" &= typ "DEBUG|INFO|WARNING|ERROR"
 } &= help "start a log monitor, monitor log entry from logServer"
 
-m_tcpSkeleton = TcpSkeleton {
+m_tcpShare = TcpShare {
     hostport = def &= help "tcp proxy server host:port" &= typ "HOST:PORT"
   , proxy = "127.0.0.1:80:80" &= help "local host to be proxy, default is 127.0.0.1:80:80" &= typ "LOCALHOST:LOCALPORT:REMOTEPORT"
   , level = "INFO" &= help "log level" &= typ "DEBUG|INFO|WARNING|ERROR"
-} &= help "proxy local tcp server, mainly used for internet access local tcp server"
+} &= help "share local tcp server, mainly used for internet access local tcp server"
 
-m_tcpStub = TcpStub {
+m_tcpConnect = TcpConnect {
     hostport = def &= help "tcp proxy server host:port" &= typ "HOST:PORT"
   , proxy = "80:80" &= help "local host to be proxy, default is 80:80" &= typ "LOCALPORT:REMOTEPORT"
   , level = "INFO" &= help "log level" &= typ "DEBUG|INFO|WARNING|ERROR"
-} &= help "proxy local tcp server, mainly used for internet access local tcp server"
+} &= help "connect to remote shared tcp server, mainly used for connect remote tcp server"
 
-mode = cmdArgsMode $ modes [m_tcpSkeleton, m_tcpStub]
-        &= help "hpay tool by 李明"
+mode = cmdArgsMode $ modes [m_tcpShare, m_tcpConnect]
+        &= help "hpay tool"
         &= program "hpay"
         &= summary ("hpay v"++ version)
 
@@ -55,12 +55,12 @@ hpayctl cmd = withSocketsDo $ do
     updateGlobalLogger rootLoggerName $ setLevel $ read $ level cmd
     infoM _name $ show cmd
     case cmd of
-        TcpSkeleton{..} -> tcpSkeleton hostport proxy
-        TcpStub{..}     -> tcpStub hostport proxy
+        TcpShare{..} -> tcpSkeleton hostport proxy
+        TcpConnect{..}     -> tcpStub hostport proxy
 
 main :: IO ()
 main = withSource $ cmdArgsRun mode >>= hpayctl
 
 -- test code
-t_skeleton  = hpayctl TcpSkeleton{hostport="127.0.0.1:9999", proxy="127.0.0.1:2222:liming/centos", level="INFO"}
-t_stub      = hpayctl TcpStub{hostport="127.0.0.1:9999", proxy="3333:liming/centos", level="INFO"}
+t_share     = hpayctl TcpShare{hostport="127.0.0.1:9999", proxy="127.0.0.1:2222:liming/centos", level="INFO"}
+t_connect   = hpayctl TcpConnect{hostport="127.0.0.1:9999", proxy="3333:liming/centos", level="INFO"}
